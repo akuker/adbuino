c6d80c094deaf2ffb69d0d7f16ebd09680eab3a0
Difegue <sugoi@cock.li>
2015-10-22 02:02:48 +0200

    updated webcam with LDAP and non-google TTS
    Signed-off-by: Difegue <sugoi@cock.li>

--- webcam/msg.pl
+++ webcam/msg.pl
@@ -3,59 +3,81 @@
 use strict;
 use CGI qw(:standard);
 use URI::Escape;
+use LWP::Simple;
+use Net::LDAP;
 
 my $qedit = new CGI;
-my $tmpdir = "/var/www/camera/picturening/";
+#my $tmpdir = "/var/www/camera/picturening/";
 my $result = "No parameter specified.";
 
-if ($qedit->param('ip') && $qedit->param('message')) 
+my @banlist = ["banned", "LDAP IDs", "go here"];
+
+if ($qedit->param('login') && $qedit->param('passw') && $qedit->param('message'))
 {
-	my $message = $qedit->param('message');
-	open(my $logfile, '>>', 'logcam.txt');
-	say $logfile ($qedit->param('ip'))." - ".$message;	
+        my $login = $qedit->param('login');
+        my $message = $qedit->param('message');
+        #open(my $logfile, '>>', 'logcam.txt');
+        #say $logfile ($qedit->param('login'))." - ".$message;
+        #$message = uri_escape($message);
 
-	#vérif IP client-> plus tard 
-
-	#vérif si y'a pas déjà un msg
-	if ($message ne "")
-	{
-		unless (-e "/var/www/camera/picturening/playing.lock")
-		{
-			my $mpdStatus = `mpc`;
-			my $paused = 0;
-			open my $fh,">/var/www/camera/picturening/playing.lock";
-			$message = uri_escape($message);
-			`wget --user-agent="" -O /var/www/camera/picturening/msg.mp3 'https://translate.google.fr/translate_tts?ie=UTF-8&q="$message"&tl=fr'`;
-			
-			if (index($mpdStatus,"playing") != -1)
-			{
-				`mpc -h epicjames1337\@localhost pause`;
-				$paused=1;
-			}
-			
-			`mpg123 /var/www/camera/picturening/msg.mp3`;
-			
-			if ($paused == 1)
-			{`mpc -h epicjames1337\@localhost play`;}
-			
-			unlink ("/var/www/camera/picturening/playing.lock");
-			$result = "Message sent!";
-		}
-		else
-		{
-			$result = "Audio message already playing."
-		}
-	}
-	else
-	{
-	$result = "Don't send empty messages faggot";
-	}
+        if (loginLDAP($login,$qedit->param('passw'))== 1)
+        {
+        	if( lc($login) ~~ @banlist )
+        		{
+        		 $result = "T bani pd";  
+        		}
+        	else
+        		{
+			open(my $logfile, '>>', 'logcam.txt');
+		        say $logfile ($qedit->param('login'))." - ".$message;
+        		$message = uri_escape($message);
+        		$result = get qq(/msg_on_speaker_server.pl?ip=$login&message=$message);
+        		}
+        }
+        else
+        {
+        	$result = "Login error.";
+        }
 }
 
 print header('text/plain');
 print $result;
 
+sub loginLDAP
+	{
+	my $login = $_[0];
+	my $pass = $_[1];
+	
+	my $ldap = Net::LDAP->new( 'ldap.eisti.fr' );
+	$ldap->start_tls();
 
+	# bind to a directory with dn and password
+	my $mesg = $ldap->bind( 'cn=webuser, dc=eisti,dc=fr',
+	                     password => 'nn' #change LDAP server adress and password here
+	                   );
 
+	$mesg = $ldap->search(
+	                        base   => 'ou=people,dc=eisti,dc=fr',
+	                        scope  => 'subtree',
+	                        filter => '(uid='.$login.')',
+	                        attrs  => ['dn']
+	                      );
 
+	# Get the user's dn and try to bind:
+	my $user_dn = $mesg->entry->dn;
+	#print $user_dn;
+	
+	my $login = $ldap->bind( $user_dn, password => $pass );
+	
+	if($login->code == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+
+	$ldap->unbind;
+	}
 
