423e44b9ada890471cc204ca1c4aa172198b8f54
Len <Len>
2016-04-02 22:46:39 +0200

    Added more shit
    Signed-off-by: Len <Len>

---
+++ rutorrent_flatui/readme.txt
@@ -0,0 +1,3 @@
+Flat theme for rtorrent
+
+Modification of https://github.com/exetico/FlatUI
--- smartwatch-workaround/smartwatch_notificationworkaround.js
+++ smartwatch-workaround/smartwatch_notificationworkaround.js
@@ -19,29 +19,42 @@ client.on('connect', function(connection) {
     });
 
     connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
+        console.log('Connection Closed');
     });
 
     connection.on('message', function(message) {
         json = JSON.parse(message.utf8Data);
+        //console.log(message.utf8Data);
 
-    if (json.type !="nop")
+    if (json.hasOwnProperty("push"))
 	    if (json.push.type == "mirror")
 	        {
-	         	console.log(json.push.title);
-	         	console.log(json.push.body);
-
+	         
 	         	if (json.push.package_name != "com.android.mms") //avoid crazy feedback loop
-	         	https.get(apiUrl+json.push.title+" "+json.push.body, function(res) {
-				    if(res != 200){
-				    	// Envoi depuis le Sim900, mail, etc.
-				    	console.log("sa a pa envoyé mdr");
-				    }
+	         	{
 
-				    res.resume();
-				}).on('error', function(e) {
-				    console.error(e);
-				});
+              console.log(json.push.title);
+              console.log(json.push.body);
+
+		         	https.get(apiUrl+json.push.title+" "+encodeURIComponent(json.push.body), function(res) {
+	
+					    //console.log(res);
+
+					    res.resume();
+
+    					}).on('error', function(e) {
+    					    console.error(e);
+    					});
+
+				    }
+            else
+            {
+              console.log("sms received");
+              //If it's a sms, we auto-dismiss it after 30 seconds. Avoids clutter on the phone while giving enough time to read.
+              //This is the best workaround we can do, since the watch doesn't allow us to dismiss notifications. (hella gay)
+              setTimeout(PostDismissal, 30000, json.push.notification_id,json.push.package_name,json.push.source_user_iden);
+
+            }
 
 	        }
         
@@ -49,3 +62,34 @@ client.on('connect', function(connection) {
 });
     
 client.connect('wss://stream.pushbullet.com/websocket/' + apiKeyPB, null);  
+
+
+function PostDismissal(notification_id,package_name,source_user_iden) {
+  // Build the post string from an object
+  var post_data = '{"push":{"notification_id":"'+notification_id+'","notification_tag":null,"package_name":"'+package_name+'","source_user_iden":"'+source_user_iden+'","type":"dismissal"},"type":"push"}';
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'api.pushbullet.com',
+      port: '443',
+      path: '/v2/ephemerals',
+      method: 'POST',
+      headers: {
+          'Access-Token': apiKeyPB,
+          'Content-Type': 'application/json'
+      }
+  };
+
+  // Set up the request
+  var post_req = https.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+}
