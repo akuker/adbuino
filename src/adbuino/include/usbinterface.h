cf3bdde866fb35985bb5ed7d6b2ffee62240d836
Tony Kuker <akuker@gmail.com>
2021-07-05 19:29:19 -0500

    Mouse works!

--- src/adbuino/include/usbinterface.h
+++ src/adbuino/include/usbinterface.h
@@ -10,85 +10,93 @@
 #endif
 #include <SPI.h>
 
-
 class MouseRptParser : public MouseReportParser
 {
+public:
+    bool MouseChanged();
+    int32_t GetDeltaX();
+    int32_t GetDeltaY();
+    void ResetMouseMovement();
+    uint16_t GetAdbRegister0();
+    bool MouseButtonIsPressed();
+
 protected:
-	void OnMouseMove	(MOUSEINFO *mi);
-	void OnLeftButtonUp	(MOUSEINFO *mi);
-	void OnLeftButtonDown	(MOUSEINFO *mi);
-	void OnRightButtonUp	(MOUSEINFO *mi);
-	void OnRightButtonDown	(MOUSEINFO *mi);
-	void OnMiddleButtonUp	(MOUSEINFO *mi);
-	void OnMiddleButtonDown	(MOUSEINFO *mi);
+    void OnMouseMove(MOUSEINFO *mi);
+    void OnLeftButtonUp(MOUSEINFO *mi);
+    void OnLeftButtonDown(MOUSEINFO *mi);
+    void OnRightButtonUp(MOUSEINFO *mi);
+    void OnRightButtonDown(MOUSEINFO *mi);
+    void OnMiddleButtonUp(MOUSEINFO *mi);
+    void OnMiddleButtonDown(MOUSEINFO *mi);
+
+private:
+    int32_t m_movedx = 0;
+    int32_t m_movedy = 0;
+    bool m_mouse_button_is_pressed = false;
 };
 
 class KbdRptParser : public KeyboardReportParser
 {
     void PrintKey(uint8_t mod, uint8_t key);
 
-  protected:
+protected:
     void OnControlKeysChanged(uint8_t before, uint8_t after);
 
-    void OnKeyDown	(uint8_t mod, uint8_t key);
-    void OnKeyUp	(uint8_t mod, uint8_t key);
+    void OnKeyDown(uint8_t mod, uint8_t key);
+    void OnKeyUp(uint8_t mod, uint8_t key);
     void OnKeyPressed(uint8_t key);
 };
 
-class UsbInterface{
-    public:
-
+class UsbInterface
+{
+public:
     void Init();
     void RunTask();
-void PrintAllAddresses(UsbDevice *pdev);
-void PrintAddress(uint8_t addr);
-void PrintDescriptors(uint8_t addr);
+    void PrintAllAddresses(UsbDevice *pdev);
+    void PrintAddress(uint8_t addr);
+    void PrintDescriptors(uint8_t addr);
 
+    void PrintAllDescriptors(UsbDevice *pdev);
 
-void PrintAllDescriptors(UsbDevice *pdev);
+    void printhubdescr(uint8_t *descrptr, uint8_t addr);
+    uint8_t getconfdescr(uint8_t addr, uint8_t conf);
+    // function to get all string descriptors
+    uint8_t getallstrdescr(uint8_t addr);
+    //  function to get single string description
+    uint8_t getstrdescr(uint8_t addr, uint8_t idx);
 
-void printhubdescr(uint8_t *descrptr, uint8_t addr);
-uint8_t getconfdescr( uint8_t addr, uint8_t conf );
-// function to get all string descriptors
-uint8_t getallstrdescr(uint8_t addr);
-//  function to get single string description
-uint8_t getstrdescr( uint8_t addr, uint8_t idx );
+    /* prints hex numbers with leading zeroes */
+    // copyright, Peter H Anderson, Baltimore, MD, Nov, '07
+    // source: http://www.phanderson.com/arduino/arduino_display.html
+    void print_hex(int v, int num_places);
 
+    /* function to print configuration descriptor */
+    void printconfdescr(uint8_t *descr_ptr);
 
-/* prints hex numbers with leading zeroes */
-// copyright, Peter H Anderson, Baltimore, MD, Nov, '07
-// source: http://www.phanderson.com/arduino/arduino_display.html
-void print_hex(int v, int num_places);
+    /* function to print interface descriptor */
+    void printintfdescr(uint8_t *descr_ptr);
 
-/* function to print configuration descriptor */
-void printconfdescr( uint8_t* descr_ptr );
+    /* function to print endpoint descriptor */
+    void printepdescr(uint8_t *descr_ptr);
 
+    /*function to print unknown descriptor */
+    void printunkdescr(uint8_t *descr_ptr);
 
-/* function to print interface descriptor */
-void printintfdescr( uint8_t* descr_ptr );
+    /* Print a string from Program Memory directly to save RAM */
+    void printProgStr(const char *str);
 
-/* function to print endpoint descriptor */
-void printepdescr( uint8_t* descr_ptr );
+    uint8_t getdevdescr(uint8_t addr, uint8_t &num_conf);
 
-/*function to print unknown descriptor */
-void printunkdescr( uint8_t* descr_ptr );
+private:
+    USB *Usb;
+    USBHub *Hub1;
+    USBHub *Hub2;
+    USBHub *Hub3;
+    USBHub *Hub4;
+    HIDBoot<USB_HID_PROTOCOL_KEYBOARD> *HidKeyboard;
+    HIDBoot<USB_HID_PROTOCOL_MOUSE> *HidMouse;
 
-/* Print a string from Program Memory directly to save RAM */
-void printProgStr(const char* str);
-
-
-uint8_t getdevdescr( uint8_t addr, uint8_t &num_conf );
-    private: 
-        USB     *Usb;
-        USBHub  *Hub1;
-        USBHub  *Hub2;
-        USBHub  *Hub3;
-        USBHub  *Hub4;
-        HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    *HidKeyboard;
-        HIDBoot<USB_HID_PROTOCOL_MOUSE>    *HidMouse;
-
-        MouseRptParser *MousePrs;
-        KbdRptParser *KeyboardPrs;
-        uint32_t next_time;
-
+    MouseRptParser *MousePrs;
+    KbdRptParser *KeyboardPrs;
+    uint32_t next_time;
 };
--- src/adbuino/src/usbmouseparser.cpp
+++ src/adbuino/src/usbmouseparser.cpp
@@ -1,33 +1,77 @@
 #include "usbinterface.h"
 
+bool MouseRptParser::MouseChanged()
+{
+    return ((m_movedx != 0) || (m_movedy != 0));
+}
+int32_t MouseRptParser::GetDeltaX()
+{
+    return m_movedx;
+}
+int32_t MouseRptParser::GetDeltaY()
+{
+    return m_movedy;
+}
+void MouseRptParser::ResetMouseMovement()
+{
+    m_movedy = 0;
+    m_movedx = 0;
+}
+bool MouseRptParser::MouseButtonIsPressed()
+{
+    return m_mouse_button_is_pressed;
+}
+
+uint16_t MouseRptParser::GetAdbRegister0()
+{
+    uint16_t reg_value = 0;
+    // Bit 15 = Button Status; 0=down
+    if (!MouseButtonIsPressed())
+    {
+        reg_value |= (1 << 15);
+    }
+    // Bit 7 = Not used. Always 1
+    reg_value |= (1 << 7);
+    // Bits 14-8 = Y move Counts (Two's compliment. Negative = up, positive = down)
+    reg_value |= ((GetDeltaY() & 0x7F) << 8);
+
+    // Bits 6-0 = X move counts (Two's compliment. Negative = left, positive = right)
+    reg_value |= ((GetDeltaX() & 0x7F) << 0);
+    return reg_value;
+}
+
 void MouseRptParser::OnMouseMove(MOUSEINFO *mi)
 {
     Serial.print("dx=");
     Serial.print(mi->dX, DEC);
     Serial.print(" dy=");
     Serial.println(mi->dY, DEC);
+    m_movedy += (int32_t)mi->dY;
+    m_movedx += (int32_t)mi->dX;
 };
-void MouseRptParser::OnLeftButtonUp	(MOUSEINFO *mi)
+void MouseRptParser::OnLeftButtonUp(MOUSEINFO *mi)
 {
     Serial.println("L Butt Up");
+    m_mouse_button_is_pressed = false;
 };
-void MouseRptParser::OnLeftButtonDown	(MOUSEINFO *mi)
+void MouseRptParser::OnLeftButtonDown(MOUSEINFO *mi)
 {
     Serial.println("L Butt Dn");
+    m_mouse_button_is_pressed = true;
 };
-void MouseRptParser::OnRightButtonUp	(MOUSEINFO *mi)
+void MouseRptParser::OnRightButtonUp(MOUSEINFO *mi)
 {
     Serial.println("R Butt Up");
 };
-void MouseRptParser::OnRightButtonDown	(MOUSEINFO *mi)
+void MouseRptParser::OnRightButtonDown(MOUSEINFO *mi)
 {
     Serial.println("R Butt Dn");
 };
-void MouseRptParser::OnMiddleButtonUp	(MOUSEINFO *mi)
+void MouseRptParser::OnMiddleButtonUp(MOUSEINFO *mi)
 {
     Serial.println("M Butt Up");
 };
-void MouseRptParser::OnMiddleButtonDown	(MOUSEINFO *mi)
+void MouseRptParser::OnMiddleButtonDown(MOUSEINFO *mi)
 {
     Serial.println("M Butt Dn");
 };
