cf5345709363693f3157c949d11f4789ed29d350
Morio <morio.earth@gmail.com>
2022-10-20 09:58:11 -0700

    Add the rest of the copyright headers

--- src/adbuino/lib/adbuino/include/adb_platform.h
+++ src/adbuino/lib/adbuino/include/adb_platform.h
@@ -1,3 +1,27 @@
+//---------------------------------------------------------------------------
+//
+//	ADBuino ADB keyboard and mouse adapter
+//
+//	   Copyright (C) 2017 bbraun
+//	   Copyright (C) 2021-2022 akuker
+//
+//  This file is part of the ADBuino project.
+//
+//  This file is free software: you can redistribute it and/or modify it under 
+//  the terms of the GNU General Public License as published by the Free 
+//  Software Foundation, either version 3 of the License, or (at your option) 
+//  any later version.
+//
+//  This file is distributed in the hope that it will be useful, but WITHOUT ANY 
+//  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+//  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+//  details.
+//
+//  You should have received a copy of the GNU General Public License along 
+//  with the file. If not, see <https://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
 #pragma once
 
 #include "Arduino.h"
