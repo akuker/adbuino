d4ab727a3f7387635978b72e65c1a95bea84309c
Tony Kuker <akuker@gmail.com>
2023-04-15 21:19:21 -0500

    Receives some stuff now

--- src/hello/hello_serial.c
+++ src/hello/hello_serial.c
@@ -10,7 +10,8 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "adb.pio.h"
+#include "adb_receive.pio.h"
+// #include "adb_write.pio.h"
 
 #define NELEMENTS(x) (sizeof(x) / sizeof(x[0]))
 
@@ -28,7 +29,7 @@ void push_word(uint16_t val)
     // The PIO state machine will "flip" the 32 bit word
     pio_sm_put(pio0, 0, ((uint32_t)val) << 16);
 }
-
+ 
 
 int main()
 {
@@ -38,13 +39,26 @@ int main()
     // todo get free sm
     PIO pio = pio0;
     int sm = 0;
-    uint offset = pio_add_program(pio, &adb_write_program);
+    // uint offset = pio_add_program(pio, &adb_write_program);
+     uint offset = pio_add_program(pio, &adb_receive_program);
 
-    adb_write_program_init(pio, sm, offset, ADB_PIN, 800000, 0);
+    adb_receive_program_init(pio, sm, offset, ADB_PIN, 800000, 0);
 
     // uint16_t t = 0;
+    int slow=0;
     while (1)
     {
+        if(!pio_sm_is_rx_fifo_empty(pio0, 0)){
+            uint32_t value = pio_sm_get(pio, 0);
+            slow+=20;
+            if(slow > 10){
+                slow = 0;
+                printf("%04X\n", value);
+                fflush(stdout);
+            }
+        }
+    }
+    while(1){
         // push_byte(t & 0xFF);
         push_word(0xABCD);
 
